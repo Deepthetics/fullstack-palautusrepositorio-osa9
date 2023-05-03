1129ad888fc834bf438469362b9c11667d44abae
Vili Sirkeoja <vili.sirkeoja@gmail.com>
2023-05-03 16:26:53 +0300

    Exercise 9.22

--- patientor/server/src/types.ts
+++ patientor/server/src/types.ts
@@ -1,11 +1,57 @@
-export interface Diagnose {
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
 }
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcare extends BaseEntry {
+  type: 'OccupationalHealthcare'
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+export type Entry = HealthCheckEntry | HospitalEntry | OccupationalHealthcare;
+
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other'
 }
 
 export interface Patient {
@@ -21,9 +67,3 @@ export interface Patient {
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 
 export type NewPatient = Omit<Patient, 'id'>;
-
-export enum Gender {
-  Male = 'male',
-  Female = 'female',
-  Other = 'other'
-}
